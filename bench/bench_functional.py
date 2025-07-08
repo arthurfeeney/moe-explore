c8bbc6782396114bf5b5dbb97979c6957d632dbf
Arthur Feeney <afeeney@uci.edu>
2025-07-08 13:09:52 -0700

    Improve benchmark

--- bench/bench_functional.py
+++ bench/bench_functional.py
@@ -21,34 +21,47 @@ def generate_inputs(
     expert_weights2 = torch.randn((num_experts, hidden_dim, input_dim), device=device, dtype=dtype) / math.sqrt(hidden_dim)
     return (input, router_weight, expert_weights1, expert_weights2)
 
-num_experts = 64
-seq_len = 2048
-hidden_dim = 256
-input_dim = 256
-activation = torch.nn.functional.gelu
-
-configs = []
-configs.append(
-    Benchmark(
+def moe_benchmark(plot_name, num_experts, seq_len, hidden_dim, input_dim, activation):
+    return Benchmark(
         x_names=["topk"],
-        x_vals=[2, 4, 8, 16, 32, 60],
+        x_vals=[2, 4, 8, 16],
         line_arg="provider",
         line_vals=["naive", "group_gemm", "fused"],
         line_names=["Naive", "Group GEMM", "Fused"],
         styles=[("green", "-"), ("blue", "-"), ("red", "-")],
         ylabel="ms",
-        plot_name="MoE Performance",
+        plot_name=plot_name,
         args={
             "num_experts": num_experts,
             "seq_len": seq_len,
             "hidden_dim": hidden_dim,
-            "input_dim": input_dim
-        }
-    )
-)
+            "input_dim": input_dim,
+            "activation": activation 
+        })
+
+configs = []
+configs.append(
+        moe_benchmark(
+            "Small Input MoE Forward",
+            num_experts=64,
+            seq_len=2048,
+            hidden_dim=256,
+            input_dim=256,
+            activation=torch.nn.functional.gelu
+        ))
+configs.append(
+        moe_benchmark(
+            "Large Input MoE Forward",
+            num_experts=64,
+            seq_len=16 * 2048,
+            hidden_dim=512,
+            input_dim=512,
+            activation=torch.nn.functional.gelu
+        ))
+
 
 @perf_report(configs)
-def benchmark(num_experts, seq_len, hidden_dim, input_dim, topk, provider):
+def benchmark_moe_forward(num_experts, seq_len, hidden_dim, input_dim, activation, topk, provider):
     input, router_weight, expert_weights1, expert_weights2 = generate_inputs(
             "cuda", torch.float16, seq_len, input_dim, hidden_dim, num_experts)
 
@@ -59,17 +72,18 @@ def benchmark(num_experts, seq_len, hidden_dim, input_dim, topk, provider):
     elif provider == "fused":
         func = topk_moe_matmul_gather_scatter_forward
 
-    quantiles = [0.5, 0.2, 0.8]
-    ms, min_ms, max_ms = do_bench(lambda: func(
-        input,
-        router_weight,
-        expert_weights1,
-        expert_weights2,
-        input_dim,
-        num_experts,
-        topk,
-        activation), quantiles=quantiles)
+    with torch.no_grad():
+        quantiles = [0.5, 0.2, 0.8]
+        ms, min_ms, max_ms = do_bench(lambda: func(
+            input,
+            router_weight,
+            expert_weights1,
+            expert_weights2,
+            input_dim,
+            num_experts,
+            topk,
+            activation), quantiles=quantiles)
 
     return ms, min_ms, max_ms
 
-benchmark.run(print_data=True, save_path="./")
+benchmark_moe_forward.run(print_data=True)
