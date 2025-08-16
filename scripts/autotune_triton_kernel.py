fe89faf4fcacec2434b137054d16dcb89610339d
Arthur Feeney <afeeney@uci.edu>
2025-08-15 17:19:02 -0700

    Add grouped_glu to autotune script

--- scripts/autotune_triton_kernel.py
+++ scripts/autotune_triton_kernel.py
@@ -3,16 +3,18 @@ This script is used to run autotuning and determine some decent default
 configuratios, so autotuning doesn't have to be run every time.
 """
 
+import argparse
 import math
 import torch
 from moe_explore.triton_kernels.autotune_config import AutotuneMode
-from moe_explore.triton_kernels.fused_moe import fused_moe, m_grouped_gemm_persistent_kernel, FusedMoeParams
+from moe_explore.triton_kernels.fused_moe import fused_moe, FusedMoeParams
+from moe_explore.triton_kernels.glu import glu, GLUParams
 from moe_explore.expert_permute import get_token_indices
 from moe_explore.testing import random_routing
 
 import os
 
-def autotune_gather(
+def autotune_grouped_gemm_gather(
     num_tokens,
     num_experts,
     K,
@@ -43,7 +45,7 @@ def autotune_gather(
     
     fused_moe(input, params, AutotuneMode.FAST)
     
-def autotune_scatter(
+def autotune_grouped_gemm_scatter(
     num_tokens_times_topk,
     num_experts,
     K,
@@ -78,19 +80,65 @@ def autotune_scatter(
     )
 
     fused_moe(input, params, AutotuneMode.FAST)
+    
+def autotune_grouped_glu_gather(
+    num_tokens,
+    num_experts,
+    K,
+    N,
+    topk,
+    dtype
+):
+    input = torch.randn((num_tokens, K), dtype=dtype, device="cuda")
+    gate_weight = torch.randn((num_experts, K, N), dtype=dtype, device="cuda") / math.sqrt(N) 
+    up_weight = torch.randn((num_experts, K, N), dtype=dtype, device="cuda") / math.sqrt(N) 
+    _, topk_indices = random_routing(num_tokens, num_experts, topk, device="cuda", dtype=dtype)
+    p = get_token_indices(
+        topk_indices.view(-1),
+        topk,
+        num_experts,
+        zero_prefix=True
+    )   
+
+    params = GLUParams(
+        gate_weight,
+        up_weight,
+        p.group_indices,
+        p.indices,
+        True,
+        num_tokens,
+        topk,
+        "silu"
+    )
+    
+    glu(input, params, AutotuneMode.FAST)
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--kernel", 
+        type=str, 
+        required=True, 
+        choices=["gather", "scatter", "glu-gather"])
+    parser.add_argument("--num-tokens", type=int, required=True)
+    args = parser.parse_args()
+
     os.environ["TRITON_PRINT_AUTOTUNING"] = "1"
 
-    num_tokens = 32000
+    num_tokens = args.num_tokens
     num_experts = 128
     K = 2048
     N = 768
     topk = 8
     dtype = torch.bfloat16
-
-    #autotune_gather(num_tokens, num_experts, K, N, topk, dtype)
-    autotune_scatter(num_tokens, num_experts, K, N, topk, dtype)
     
+    if args.kernel == "gather":
+        autotune_grouped_gemm_gather(num_tokens, num_experts, K, N, topk, dtype)
+    elif args.kernel == "scatter":
+        autotune_grouped_gemm_scatter(num_tokens, num_experts, K, N, topk, dtype)
+    elif args.kernel == "glu-gather":
+        autotune_grouped_glu_gather(num_tokens, num_experts, K, N, topk, dtype)
+    
+
 if __name__ == "__main__":
     main()
