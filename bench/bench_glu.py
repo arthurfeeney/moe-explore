f85f85cfc18d352a3fccb616483b37c91a074dd6
Arthur Feeney <afeeney@uci.edu>
2025-08-18 14:14:20 -0700

    add scattermoe to bench_glu

--- bench/bench_glu.py
+++ bench/bench_glu.py
@@ -17,11 +17,12 @@ sys.path.append("bench/")
 sys.path.append("bench/external")
 
 try:
-    from scattermoe.mlp import MLP as ScatterMoEMLP
+    from scattermoe.mlp import GLUMLP as ScatterMoEGLU
+    from external.scattermoe import scattermoe_forward
     HAVE_SCATTERMOE = True
 except ImportError:
     HAVE_SCATTERMOE = False
-#from external.scattermoe import scattermoe_forward
+    
 #from external.sglang_fused_moe import sglang_fused_moe
 #except ImportError:
 #    print("ScatterMoE and SGLang not installed, skipping")
@@ -43,15 +44,15 @@ def glu_tflops(num_tokens, num_experts, input_dim, hidden_dim, act_experts, ms):
     flop_per_sec = tera_flop_count / (ms / 1000)
     return flop_per_sec
 
-def moe_benchmark(plot_name, num_experts, act_experts, seq_len, hidden_dim, input_dim, activation):
+def moe_benchmark(plot_name, num_experts, act_experts, hidden_dim, input_dim, activation):
     line_vals = ["torch", "grouped_gemm", "fused"]
-    line_names = ["Torch", "Grouped GEMM", "Fused"]
+    line_names = ["Torch", "Grouped GLU", "Fused GLU"]
     if HAVE_SCATTERMOE:
         line_vals.append("scattermoe")
         line_names.append("ScatterMoE")
     return Benchmark(
-        x_names=["batch_size"],
-        x_vals=[1, 2, 4, 8, 16, 32, 64],
+        x_names=["seq_len"],
+        x_vals=[64, 128, 256, 512, 1024, 2048, 4096],
         line_arg="provider",
         line_vals=line_vals,
         line_names=line_names,
@@ -67,7 +68,6 @@ def moe_benchmark(plot_name, num_experts, act_experts, seq_len, hidden_dim, inpu
         args={
             "num_experts": num_experts,
             "act_experts": act_experts,
-            "seq_len": seq_len,
             "hidden_dim": hidden_dim,
             "input_dim": input_dim,
             "activation": activation 
@@ -76,20 +76,18 @@ def moe_benchmark(plot_name, num_experts, act_experts, seq_len, hidden_dim, inpu
 configs = []
 configs.append(
         moe_benchmark(
-            "Qwen3-30B-A3B, seq_len=64, 8/128 experts",
+            "Qwen3-30B-A3B_seq_len=64_experts=8_128",
             num_experts=128,
             act_experts=8,
-            seq_len=64,
             input_dim=2048,
             hidden_dim=768,
             activation="silu"
         ))
 configs.append(
         moe_benchmark(
-            "OLMoE-1B-7B, seq_len=64, 8/64 experts",
+            "OLMoE-1B-7B_seq_len=64_experts=8_64",
             num_experts=64,
             act_experts=8,
-            seq_len=64,
             input_dim=2048,
             hidden_dim=1024,
             activation="silu"
@@ -99,11 +97,10 @@ configs.append(
 def benchmark_moe_forward(
     num_experts,
     act_experts,
-    seq_len, 
     hidden_dim, 
     input_dim, 
     activation, 
-    batch_size, 
+    seq_len, 
     provider
 ):
     glu_params = random_glu(
@@ -119,7 +116,7 @@ def benchmark_moe_forward(
         num_experts=num_experts,
         topk=act_experts
     )
-    input = torch.randn((batch_size * seq_len, input_dim), device=torch.device("cuda"), dtype=torch.bfloat16)
+    input = torch.randn((seq_len, input_dim), device=torch.device("cuda"), dtype=torch.bfloat16)
 
     quantiles = [0.5, 0.2, 0.8]
     autotune_mode = AutotuneMode.NONE
@@ -129,7 +126,16 @@ def benchmark_moe_forward(
         ms, min_ms, max_ms = do_bench(lambda: moe_glu_grouped_gemm(input, moe_params, autotune_mode), quantiles=quantiles)
     elif provider == "fused":
         ms, min_ms, max_ms = do_bench(lambda: moe_glu_grouped_gemm_fused(input, moe_params, autotune_mode), quantiles=quantiles)
+    elif provider == "scattermoe":
+        glu = ScatterMoEGLU(
+            input_size=input_dim, 
+            hidden_size=hidden_dim,
+            num_experts=moe_params.num_experts,
+            top_k=moe_params.topk).to("cuda").to(torch.bfloat16)
+        ms, min_ms, max_ms = do_bench(
+            lambda: scattermoe_forward(input, moe_params.expert_params.router_weight, glu, moe_params.topk),
+            quantiles=quantiles)
 
     return ms, min_ms, max_ms
 
-benchmark_moe_forward.run(print_data=True)
+benchmark_moe_forward.run(print_data=True, save_path="./")
